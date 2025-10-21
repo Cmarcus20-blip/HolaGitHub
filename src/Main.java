e691e4d58fbc965c4eb27995961a13a699a3b886
Cmarcus20-blip <marcuscampan1@gmail.com>
2025-10-21 12:33:53 +0200

    Versión inicial del programa HolaMundoGitHub

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Programa HolaMundoGitHub
+ * Muestra un mensaje de saludo por pantalla.
+ * Autor: Marcus Campan
+ * Fecha: 21/10/2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, GitHub! Este es mi primer repositorio en Java.");
+    }
+}
