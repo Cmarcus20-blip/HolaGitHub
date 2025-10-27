32b0ef78b8bf659a529b6b4f45694eb7e16ceefa
Cmarcus20-blip <marcuscampan1@gmail.com>
2025-10-27 20:57:59 +0100

    Merge remote-tracking branch 'origin/main'

--- src/EJERCICIOS/Ejercicio_10.java
+++ src/EJERCICIOS/Ejercicio_10.java
@@ -11,8 +11,5 @@ public class Ejercicio_10 {
         if (edad >= 18) {
             System.out.println("Eres mayor de edad");
             }
-        else {
-            System.out.println("Eres menor de edad");
-        }
     }
 }
